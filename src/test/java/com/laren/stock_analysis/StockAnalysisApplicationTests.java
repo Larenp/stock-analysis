a8db7b615aa430d7a565c7188b535d33a3583931
Laren <pintolaren05@gmail.com>
2025-11-19 20:44:00 +0530

    Initial Spring Boot setup

---
+++ src/test/java/com/laren/stock_analysis/StockAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laren.stock_analysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockAnalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
